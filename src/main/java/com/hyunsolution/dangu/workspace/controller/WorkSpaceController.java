79de8500ef7a3c931dfd722844ac75b1cd4b7ee2
Jin Hyuk <ekrrdj07@gmail.com>
2024-10-12 02:52:23 +0900

    style: 워크스페이스 Operation 설정

--- src/main/java/com/hyunsolution/dangu/workspace/controller/WorkSpaceController.java
+++ src/main/java/com/hyunsolution/dangu/workspace/controller/WorkSpaceController.java
@@ -2,6 +2,7 @@ package com.hyunsolution.dangu.workspace.controller;
 
 import com.hyunsolution.dangu.workspace.dto.response.GetWorkspacesResponse;
 import com.hyunsolution.dangu.workspace.service.WorkspaceService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,11 +15,13 @@ public class WorkSpaceController {
 
     // TODO: @다다 님이 ApiResponse 개발하면 수정할 예정
     @PostMapping("/workspace")
+    @Operation(summary = "워크스페이스(매칭) 생성", description = "워크스페이스(매칭)를 생성한다.")
     public void addWorkSpace(@RequestHeader("Authorization") String id) {
         workSpaceService.addWorkspace(Long.valueOf(id));
     }
 
     @GetMapping("/workspaces")
+    @Operation(summary = "워크스페이스(매칭) 목록조회", description = "매칭이 완료되지 않고 대기 하고 있는 매칭만 조회합니다.")
     public List<GetWorkspacesResponse> getWorkspaces() {
         return workSpaceService.getWorkspaces();
     }
