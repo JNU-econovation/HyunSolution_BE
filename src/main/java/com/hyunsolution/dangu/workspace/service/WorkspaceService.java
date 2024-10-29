52f07bc467e8673a213a93287093223a8031357a
Lee Da-hyeon <dlekgus0205@naver.com>
2024-10-30 01:50:53 +0900

    feat: WorkspaceService 로직 수정 - filter 날짜 로직 추가

--- src/main/java/com/hyunsolution/dangu/workspace/service/WorkspaceService.java
+++ src/main/java/com/hyunsolution/dangu/workspace/service/WorkspaceService.java
@@ -6,7 +6,11 @@ import com.hyunsolution.dangu.user.exception.UserNotFoundException;
 import com.hyunsolution.dangu.workspace.domain.Workspace;
 import com.hyunsolution.dangu.workspace.domain.WorkspaceRepository;
 import com.hyunsolution.dangu.workspace.dto.response.GetWorkspacesResponse;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +27,11 @@ public class WorkspaceService {
     }
 
     public List<GetWorkspacesResponse> getWorkspaces() {
+        LocalDateTime dateFilter = LocalDateTime.now().minusDays(1);
         return workSpaceRepository.findAll().stream()
                 .filter(workspace -> !workspace.isMatched())
-                .map(
-                        workspace ->
-                                GetWorkspacesResponse.of(
-                                        workspace.getId(), workspace.getCreator().getUid()))
+                .filter(workspace -> workspace.getCreatedAt().isBefore(dateFilter)) //게임방 조회: 유지 시간은 24h
+                .map(workspace ->GetWorkspacesResponse.of(workspace.getId(), workspace.getCreator().getUid()))
                 .toList();
     }
 }
