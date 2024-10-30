f5662d612d40de4c6106128bceadf1d8c9fc5c35
Lee Da-hyeon <dlekgus0205@naver.com>
2024-10-31 01:17:15 +0900

    feat:ParticipantService 예외 처리 - Optional에 대한 orElseThrow처리

--- src/main/java/com/hyunsolution/dangu/participant/service/ParticipantService.java
+++ src/main/java/com/hyunsolution/dangu/participant/service/ParticipantService.java
@@ -1,11 +1,12 @@
 package com.hyunsolution.dangu.participant.service;
 
-import com.hyunsolution.dangu.participant.Participant;
+import com.hyunsolution.dangu.participant.domain.Participant;
 import com.hyunsolution.dangu.participant.domain.ParticipantRepository;
 import com.hyunsolution.dangu.user.domain.UserRepository;
 import com.hyunsolution.dangu.workspace.domain.Workspace;
 import com.hyunsolution.dangu.workspace.domain.WorkspaceRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,25 +22,24 @@ public class ParticipantService {
     @Transactional
     public void changeMatching(Long id, Long workspaceId) {
         // 상태 변경
+        Participant participantOptional =
+                participantRepository.findByUserIdAndWorkspaceId(id, workspaceId)
+                        .orElseThrow(()-> new NoSuchElementException("Participant not found"));
 
-        Optional<Participant> participantOptional =
-                participantRepository.findByUserIdAndWorkspaceId(id, workspaceId);
-        participantOptional.get().accept();
-        //        participantRepository.updateByIdAndWorkspace(id, workspaceId);
+        participantOptional.accept();
 
         // participant테이블에서 roomNumber로 들어온 숫자를 통해 누가 있는지 파악
-        List<Long> participants = participantRepository.findByVisitorMatch(workspaceId);
+        List<Long> participantIds = participantRepository.findParticipantIdByWorkspaceId(workspaceId);
 
-        for (Long participant : participants) {
-            boolean mathingCheck = participantRepository.existsById(participant);
+        // 방안에 모든 참가자가 "확정"버튼을 눌렀는지 확인
+        for (Long participant : participantIds) {
+            boolean mathingCheck = participantRepository.isClickedById(participant);
             if (!mathingCheck) {
                 return;
             }
         }
-        // 방안에 모든 참가자가 "확정"버튼을 눌렀는지 확인하고 게임방 테이블 속 매칭 결과를 true로 바꿈
-        Workspace workspace1 = workspaceRepository.findById(workspaceId).get();
-        workspace1.finalAccept();
-        //        workspaceRepository.UpdateWorkspaceStatus(workspaceId);
-
+        // 게임방 테이블 속 매칭 결과를 true로 바꿈
+        Workspace workspace1 = workspaceRepository.findById(workspaceId).orElseThrow(()-> new NoSuchElementException("Workspace not found"));
+        workspace1.acceptFinal();
     }
 }
