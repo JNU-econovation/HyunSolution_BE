7913adfa2bc5f5b74191d142e00d2b167aaad949
Lee Da-hyeon <dlekgus0205@naver.com>
2024-10-31 01:16:14 +0900

    feat: ParticipantRepository 메소드 명 수정 - findByVisitorMatch->findParticipantIdByWorkspaceId - existsById->isClickedById

--- src/main/java/com/hyunsolution/dangu/participant/domain/ParticipantRepository.java
+++ src/main/java/com/hyunsolution/dangu/participant/domain/ParticipantRepository.java
@@ -1,6 +1,5 @@
 package com.hyunsolution.dangu.participant.domain;
 
-import com.hyunsolution.dangu.participant.Participant;
 import com.hyunsolution.dangu.user.domain.User;
 import java.util.List;
 import java.util.Optional;
@@ -12,13 +11,11 @@ public interface ParticipantRepository extends JpaRepository<User, Long> {
 
     // 같은 방에 있는 사람들 id(PK) 리스트
     @Query(value = "SELECT p.id FROM Participant p WHERE p.workspace.id=:workspaceId")
-    List<Long> findByVisitorMatch(Long workspaceId);
+    List<Long> findParticipantIdByWorkspaceId(Long workspaceId);
 
     // ID별 매칭버튼 클릭 여부
-    @Query(
-            value =
-                    "SELECT COUNT(p) >0 FROM Participant p WHERE p.id=:id AND p.participantMatch=true")
-    Boolean existsById(long id);
+    @Query(value = "SELECT COUNT(p) >0 FROM Participant p WHERE p.id=:id AND p.participantMatch=true")
+    Boolean isClickedById(long id);
 
     // 개인 ID 찾기
     @Query(
