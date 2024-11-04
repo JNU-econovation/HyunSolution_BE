2f84ff28f040ec9595111139ce7d54bb6852aade
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-04 21:38:08 +0900

    feat: LoginResponse 작성

---
+++ src/main/java/com/hyunsolution/dangu/user/dto/response/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.hyunsolution.dangu.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponse {
+    private Long id;
+}
