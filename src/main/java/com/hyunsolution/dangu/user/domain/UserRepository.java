7ece7df0bfa877b8e5f3d5bb02b7d33dcb7df945
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-04 23:05:40 +0900

    feat: UserRepository 수정 - userRepository.findByUid의 반환타입 Optional로 수정

--- src/main/java/com/hyunsolution/dangu/user/domain/UserRepository.java
+++ src/main/java/com/hyunsolution/dangu/user/domain/UserRepository.java
@@ -1,7 +1,8 @@
 package com.hyunsolution.dangu.user.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUid(String uid);
+    Optional<User> findByUid(String uid);
 }
