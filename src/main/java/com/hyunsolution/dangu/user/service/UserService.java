812a9c14e5cb5da04108b19f84a01d11ce4f86bc
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-04 21:38:48 +0900

    feat: UserService 작성 - login과 registerUser 생성

---
+++ src/main/java/com/hyunsolution/dangu/user/service/UserService.java
@@ -0,0 +1,37 @@
+package com.hyunsolution.dangu.user.service;
+
+import com.hyunsolution.dangu.user.domain.User;
+import com.hyunsolution.dangu.user.domain.UserRepository;
+import com.hyunsolution.dangu.user.dto.response.LoginResponse;
+import com.hyunsolution.dangu.user.exception.UserNotFoundException;
+import com.hyunsolution.dangu.user.exception.UserWrongPasswordException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    // TODO: 왜 트랜잭션 어노테이션을 붙이는지 공부하기(with flush가 언제 일어나나) - 다현
+    public LoginResponse login(String uid, String password) {
+        User loginUser = userRepository.findByUid(uid);
+        // 사용자 존재 여부 판단
+        if (loginUser == null) {
+            User newUser = registerUser(uid, password);
+            return new LoginResponse(newUser.getId());
+        }
+        // 비밀번호 일치 확인
+        if (loginUser.getPassword().equals(password)) {
+            return new LoginResponse(loginUser.getId());
+        } else {
+            throw UserWrongPasswordException.USER_WRONG_PASSWORD_EXCEPTION;
+        }
+    }
+
+    public User registerUser(String username, String password) {
+        return userRepository.save(new User(username, password));
+    }
+}
