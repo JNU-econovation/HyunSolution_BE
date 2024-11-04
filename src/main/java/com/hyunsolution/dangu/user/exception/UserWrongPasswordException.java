c63430237175211ad5aca6e474c810e21390914e
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-04 23:02:55 +0900

    feat: UserWrongPasswordException 추가 수정

--- src/main/java/com/hyunsolution/dangu/user/exception/UserWrongPasswordException.java
+++ src/main/java/com/hyunsolution/dangu/user/exception/UserWrongPasswordException.java
@@ -3,9 +3,10 @@ package com.hyunsolution.dangu.user.exception;
 import com.hyunsolution.dangu.common.exception.CustomException;
 
 public class UserWrongPasswordException extends CustomException {
-    public static final UserWrongPasswordException USER_WRONG_PASSWORD_EXCEPTION = new UserWrongPasswordException();
+    public static final UserWrongPasswordException USER_WRONG_PASSWORD_EXCEPTION =
+            new UserWrongPasswordException();
 
     private UserWrongPasswordException() {
-        super(UserWrongPasswordError.USER_WRONG_PASSWORD);
+        super(UserError.USER_WRONG_PASSWORD);
     }
 }
