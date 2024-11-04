c41c43a44139350caf612979849a073aa54fb06c
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-04 23:03:45 +0900

    feat: UserService 추가 수정 - userRepository.findByUid의 반환타입 Optional로 수정

--- src/main/java/com/hyunsolution/dangu/user/service/UserService.java
+++ src/main/java/com/hyunsolution/dangu/user/service/UserService.java
@@ -3,8 +3,8 @@ package com.hyunsolution.dangu.user.service;
 import com.hyunsolution.dangu.user.domain.User;
 import com.hyunsolution.dangu.user.domain.UserRepository;
 import com.hyunsolution.dangu.user.dto.response.LoginResponse;
-import com.hyunsolution.dangu.user.exception.UserNotFoundException;
 import com.hyunsolution.dangu.user.exception.UserWrongPasswordException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,25 +17,21 @@ public class UserService {
     @Transactional
     // TODO: 왜 트랜잭션 어노테이션을 붙이는지 공부하기(with flush가 언제 일어나나) - 다현
     public LoginResponse login(String uid, String password) {
-        User loginUser = userRepository.findByUid(uid);
+        Optional<User> loginUser = userRepository.findByUid(uid);
         // 사용자 존재 여부 판단
-        if (loginUser == null) {
+        if (loginUser.isEmpty()) {
             User newUser = registerUser(uid, password);
             return new LoginResponse(newUser.getId());
         }
         // 비밀번호 일치 확인
-        if (loginUser.getPassword().equals(password)) {
-            return new LoginResponse(loginUser.getId());
+        if (loginUser.get().getPassword().equals(password)) {
+            return new LoginResponse(loginUser.get().getId());
         } else {
             throw UserWrongPasswordException.USER_WRONG_PASSWORD_EXCEPTION;
         }
     }
 
     public User registerUser(String uid, String password) {
-        return userRepository.save(User.
-                builder()
-                .uid(uid)
-                .password(password)
-                .build());
+        return userRepository.save(User.builder().uid(uid).password(password).build());
     }
 }
