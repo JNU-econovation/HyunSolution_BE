e6f17e658ed20dd9744e7f3b8fc253c04f39b8be
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-07 01:39:44 +0900

    feat:Merge Confilcts 해결

--- src/main/java/com/hyunsolution/dangu/user/domain/UserRepository.java
+++ src/main/java/com/hyunsolution/dangu/user/domain/UserRepository.java
@@ -2,6 +2,8 @@ package com.hyunsolution.dangu.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUid(String uid);
+    Optional<User> findByUid(String uid);
 }
