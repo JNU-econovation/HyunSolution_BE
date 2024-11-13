76f67a05b81c2503f0fc13c8ebd66d508819f8ba
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-14 02:57:33 +0900

    feat: ParticipantController 작성 - /participant/{roomNumber} 추가

--- src/main/java/com/hyunsolution/dangu/participant/controller/ParticipantController.java
+++ src/main/java/com/hyunsolution/dangu/participant/controller/ParticipantController.java
@@ -2,6 +2,7 @@ package com.hyunsolution.dangu.participant.controller;
 
 import com.hyunsolution.dangu.common.apiResponse.ApiResponse;
 import com.hyunsolution.dangu.participant.domain.ParticipantRepository;
+import com.hyunsolution.dangu.participant.dto.response.EnterChatRoomResponse;
 import com.hyunsolution.dangu.participant.service.ParticipantService;
 import com.hyunsolution.dangu.user.domain.UserRepository;
 import com.hyunsolution.dangu.workspace.domain.WorkspaceRepository;
@@ -21,6 +22,7 @@ public class ParticipantController {
     private final WorkspaceRepository workspaceRepository;
     private final ParticipantService participantService;
 
+    //사용자 매칭 버튼 클릭
     @PostMapping("/participant/matching/{roomNumber}")
     public ApiResponse<?> matching(
             @Parameter(hidden = true) @RequestHeader("Authorization") Long id,
@@ -29,5 +31,14 @@ public class ParticipantController {
         // 매칭확인버튼 누른 사용자 매칭칼럼 상태 변경
         participantService.changeMatching(id, workspaceId);
         return ApiResponse.success(null);
+    }
+
+    // 사용자 채팅방 입장
+    @PostMapping("/participant/{roomNumber}")
+    public ApiResponse<EnterChatRoomResponse> enterChatRoom(
+            @Parameter(hidden = true) @RequestHeader("Authorization") Long id,
+            @PathVariable("roomNumber") Long workspaceId) {
+        EnterChatRoomResponse response = participantService.sendEnteringMessage(id, workspaceId);
+        return ApiResponse.success(response);
     }
 }
