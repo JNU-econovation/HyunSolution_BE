af7a5695f3ab5cfcd8600ce63dbb63551a202505
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-13 23:46:27 +0900

    feat: ChatError 적용

--- src/main/java/com/hyunsolution/dangu/chatting/service/ChatService.java
+++ src/main/java/com/hyunsolution/dangu/chatting/service/ChatService.java
@@ -1,10 +1,12 @@
-package com.hyunsolution.dangu.chatting.dto.service;
+package com.hyunsolution.dangu.chatting.service;
 
 import com.hyunsolution.dangu.chatting.domain.ChatRepository;
 import com.hyunsolution.dangu.chatting.domain.Chatting;
 import com.hyunsolution.dangu.chatting.dto.response.ChatMessageDetailResponse;
+import com.hyunsolution.dangu.chatting.exception.ChatRoomNotFoundException;
 import com.hyunsolution.dangu.user.domain.User;
 import com.hyunsolution.dangu.user.domain.UserRepository;
+import com.hyunsolution.dangu.user.exception.UserNotFoundException;
 import com.hyunsolution.dangu.workspace.domain.Workspace;
 import com.hyunsolution.dangu.workspace.domain.WorkspaceRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,11 +26,11 @@ public class ChatService {
         Workspace workspace =
                 workspaceRepository
                         .findById(chatRoomId)
-                        .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 채팅방 id입니다."));
+                        .orElseThrow(() ->ChatRoomNotFoundException.EXCEPTION);
         User user =
                 userRepository
                         .findById(userPk)
-                        .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자 id입니다."));
+                        .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
         Chatting chatMessage =
                 Chatting.builder().workspace(workspace).user(user).content(message).build();
