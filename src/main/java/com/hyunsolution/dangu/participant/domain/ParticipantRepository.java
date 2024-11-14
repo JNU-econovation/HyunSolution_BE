205d5b15bed6be8117080ebf652b4aca85e16323
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-15 01:02:12 +0900

    feat: 채팅 입장메시지 로직 구현

--- src/main/java/com/hyunsolution/dangu/participant/domain/ParticipantRepository.java
+++ src/main/java/com/hyunsolution/dangu/participant/domain/ParticipantRepository.java
@@ -21,4 +21,7 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
                     "SELECT p from Participant p join p.workspace w where p.user.id =:id and p.workspace.id=:workspaceId")
     Optional<Participant> findByUserIdAndWorkspaceId(
             @Param("id") Long id, @Param("workspaceId") Long workspaceId);
+
+
+    List<Participant> findByWorkspaceId(Long workspaceId);
 }
