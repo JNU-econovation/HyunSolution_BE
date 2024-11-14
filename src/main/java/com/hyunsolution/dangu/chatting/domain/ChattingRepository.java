ab3e1c8f1d8c26c708bd813e1ab1afe2140b9244
Jin Hyuk <ekrrdj07@gmail.com>
2024-11-14 15:33:56 +0900

    feat: 채팅 목록 조회

--- src/main/java/com/hyunsolution/dangu/chatting/domain/ChattingRepository.java
+++ src/main/java/com/hyunsolution/dangu/chatting/domain/ChattingRepository.java
@@ -2,7 +2,16 @@ package com.hyunsolution.dangu.chatting.domain;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChattingRepository extends JpaRepository<Chatting, Long> {
+    @Query("select c from Chatting c where c.workspace.id =:workspaceId")
     List<Chatting> findByWorkspaceId(Long workspaceId);
+
+    @Query(
+            value =
+                    "select c.content from chatting c where c.id = ("
+                            + "select MAX(id) from chatting where workspace_id = :workspaceId)",
+            nativeQuery = true)
+    String findLastChattingContentByWorkspaceId(Long workspaceId);
 }
--- src/main/java/com/hyunsolution/dangu/workspace/domain/WorkspaceRepository.java
+++ src/main/java/com/hyunsolution/dangu/workspace/domain/WorkspaceRepository.java
@@ -1,12 +1,13 @@
 package com.hyunsolution.dangu.workspace.domain;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
-
-    @Modifying
-    @Query(value = "UPDATE Workspace w SET w.isMatched=true WHERE w.id=:workspaceId")
-    void UpdateWorkspaceStatus(Long workspaceId);
+    @EntityGraph(attributePaths = {"participants"})
+    @Query(
+            "select w from Workspace w where exists (select 1 from Participant p where p.workspace = w and p.user.id = :userId)")
+    List<Workspace> findByParticipantUserId(Long userId);
 }
