7c436ba5996840c55d638e47dc9c12864fbc9a56
Jin Hyuk <ekrrdj07@gmail.com>
2024-11-22 00:26:21 +0900

    docs: Swagger Operation 수정

--- src/main/java/com/hyunsolution/dangu/participant/controller/ParticipantController.java
+++ src/main/java/com/hyunsolution/dangu/participant/controller/ParticipantController.java
@@ -23,7 +23,8 @@ public class ParticipantController {
 
     // 사용자 매칭 버튼 클릭
     @PostMapping("/participant/matching/{chatRoomId}")
-    @Operation(summary = "참가자 매칭 수락/거절", description = "참가자 매칭 수락/거절 한다. (isMatch가 true이면 매칭 수락)")
+    @Operation(summary = "참가자 매칭 수락/거절", description = "참가자 매칭 수락/거절 한다. (isMatch가 true이면 매칭 수락)" +
+            "workspace의 매칭이 확정된 경우 참여자는 매칭 거절을 할 수 없다.")
     public ApiResponse<?> matching(
             @Parameter(hidden = true) @RequestHeader("Authorization") Long id,
             @PathVariable("chatRoomId") Long chatRoomId,
