9f573aad31ea7158898d9d83dc0ed99402c9a68a
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-25 05:51:56 +0900

    feature: ChatRoomRepository 수정 -workspaceRepository에서 변경

--- src/main/java/com/hyunsolution/dangu/chatRoom/domain/ChatRoomRepository.java
+++ src/main/java/com/hyunsolution/dangu/chatRoom/domain/ChatRoomRepository.java
@@ -1,6 +1,13 @@
 package com.hyunsolution.dangu.chatRoom.domain;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    @EntityGraph(attributePaths = {"participants"})
+    @Query(
+            "select c from ChatRoom c where exists (select 1 from Participant p where p.chatRoom = c and p.user.id = :userId)")
+    List<ChatRoom> findByParticipantUserId(Long userId);
 }
