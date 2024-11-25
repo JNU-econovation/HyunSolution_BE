a730608a142462d6348b760db7bfb71cf3402a71
Lee Da-hyeon <dlekgus0205@naver.com>
2024-11-26 04:17:20 +0900

    feature: GetMatchStatusResponse dto 생성

---
+++ src/main/java/com/hyunsolution/dangu/participant/dto/response/GetMatchStatusResponse.java
@@ -0,0 +1,13 @@
+package com.hyunsolution.dangu.participant.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class GetMatchStatusResponse {
+    private boolean counterpart;
+    private boolean owner;
+    private boolean matchResult;
+}
