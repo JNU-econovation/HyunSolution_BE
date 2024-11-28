1eeee4939f83ea6ebbacdc9b21b7bf1e0d885ecd
Jin Hyuk <ekrrdj07@gmail.com>
2024-11-28 20:58:57 +0900

    docs: 스웨거용 추가 설명

--- src/main/java/com/hyunsolution/dangu/workspace/domain/WorkspaceRepository.java
+++ src/main/java/com/hyunsolution/dangu/workspace/domain/WorkspaceRepository.java
@@ -8,6 +8,8 @@ public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
     @Query(
             "select w from Workspace w where w.isMatched = false and w.createdAt > current_timestamp - 1")
     List<Workspace> findUnmatchedAndCreatedWithinLastDay();
-    @Query("select count(w.id) > 0 from Workspace w where w.creator.id = :creatorId and w.createdAt > current_timestamp - 1")
+
+    @Query(
+            "select count(w.id) > 0 from Workspace w where w.creator.id = :creatorId and w.createdAt > current_timestamp - 1")
     Boolean existsByCreatorId(Long creatorId);
 }
