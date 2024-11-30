f81518b9709f8d947002062a399cf7fd83f80805
Jin Hyuk <ekrrdj07@gmail.com>
2024-12-01 08:45:53 +0900

    feat: server, mysql 타임존 Asia/Seoul로 설정

--- src/main/java/com/hyunsolution/dangu/DanguApplication.java
+++ src/main/java/com/hyunsolution/dangu/DanguApplication.java
@@ -1,5 +1,7 @@
 package com.hyunsolution.dangu;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class DanguApplication {
+
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DanguApplication.class, args);
