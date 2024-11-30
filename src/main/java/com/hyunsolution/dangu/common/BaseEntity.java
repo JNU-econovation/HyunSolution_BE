df278a446135533626121d6a45b922d636dc9859
Lee Da-hyeon <dlekgus0205@naver.com>
2024-12-01 02:03:52 +0900

    feature: BaseEntity 수정 - @ColumnDefault("false") 추가

--- src/main/java/com/hyunsolution/dangu/common/BaseEntity.java
+++ src/main/java/com/hyunsolution/dangu/common/BaseEntity.java
@@ -3,6 +3,7 @@ package com.hyunsolution.dangu.common;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 public class BaseEntity {
     // 삭제
+    @ColumnDefault("false")
     private boolean isDeleted;
 
     public void toggleDeleted() {
