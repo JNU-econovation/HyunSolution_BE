778175e22af4d78f7b2e8632b0c7b4ca8be9edab
Lee Da-hyeon <dlekgus0205@naver.com>
2024-12-01 01:59:46 +0900

    feature: DynamicInsert 추가 - ColumnDefault 추가 - DynamicInsert 추가

--- src/main/java/com/hyunsolution/dangu/chatting/domain/Chatting.java
+++ src/main/java/com/hyunsolution/dangu/chatting/domain/Chatting.java
@@ -3,13 +3,14 @@ package com.hyunsolution.dangu.chatting.domain;
 import com.hyunsolution.dangu.chatRoom.domain.ChatRoom;
 import com.hyunsolution.dangu.common.BaseEntity;
 import com.hyunsolution.dangu.user.domain.User;
-import java.awt.*;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -18,6 +19,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 @Table(indexes = {@Index(name = "idx_chatRoom_id_id", columnList = "chatRoom_id, id DESC")})
+@DynamicInsert
 public class Chatting extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,8 +47,8 @@ public class Chatting extends BaseEntity {
     private LocalDateTime createdAt;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private MessageType messageType = MessageType.TEXT;
+    @ColumnDefault("'TEXT'")
+    private MessageType messageType;
 
     @Builder
     private Chatting(ChatRoom chatRoom, User sender, String content, MessageType messageType) {
