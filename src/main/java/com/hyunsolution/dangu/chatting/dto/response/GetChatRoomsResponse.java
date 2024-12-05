c2cd98aadbeec3b512238a1a3dc8425a76079010
Jin Hyuk <ekrrdj07@gmail.com>
2024-12-05 21:25:36 +0900

    rename: otherPerson을 otherPeople로 수정

--- src/main/java/com/hyunsolution/dangu/chatting/dto/response/GetChatRoomsResponse.java
+++ src/main/java/com/hyunsolution/dangu/chatting/dto/response/GetChatRoomsResponse.java
@@ -3,7 +3,7 @@ package com.hyunsolution.dangu.chatting.dto.response;
 import java.util.List;
 
 public record GetChatRoomsResponse(
-        Long chatRoomId, String lastMessage, List<String> otherPerson, int unReadCount) {
+        Long chatRoomId, String lastMessage, List<String> otherPeople, int unReadCount) {
     public static GetChatRoomsResponse of(
             Long chatRoomId, String lastMessage, List<String> otherPerson, int unReadCount) {
         return new GetChatRoomsResponse(chatRoomId, lastMessage, otherPerson, unReadCount);
--- src/main/java/com/hyunsolution/dangu/chatting/dto/response/GetChattingsResponse.java
+++ src/main/java/com/hyunsolution/dangu/chatting/dto/response/GetChattingsResponse.java
@@ -2,7 +2,7 @@ package com.hyunsolution.dangu.chatting.dto.response;
 
 import java.util.List;
 
-public record GetChattingsResponse(List<String> otherName, List<ChattingsDto> chattings) {
+public record GetChattingsResponse(List<String> otherPeople, List<ChattingsDto> chattings) {
     public static GetChattingsResponse of(List<String> otherName, List<ChattingsDto> chattings) {
         return new GetChattingsResponse(otherName, chattings);
     }
