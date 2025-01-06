50c6419d1f9dce0da1fc1aa3aabad965d7546bfb
Lee Da-hyeon <dlekgus0205@naver.com>
2025-01-06 22:31:45 +0900

    feat: 프론트 웹소켓 연결 시 필요한 헤더정보 수정

--- src/main/java/com/hyunsolution/dangu/common/config/webSocket/WebSocketSwaggerDocsController.java
+++ src/main/java/com/hyunsolution/dangu/common/config/webSocket/WebSocketSwaggerDocsController.java
@@ -14,6 +14,7 @@ public class WebSocketSwaggerDocsController {
             - WebSocket 접속 URL: wss://hyunsolution.duckdns.org/chat
             - STOMP 구독 주소: /topic/chat/{chatRoomId}
             - 메시지 전송 주소: /app/chat/{chatRoomId}
+            - 헤더 속 정보: "Authorization"에 사용자 id, "RoomId"에 방번호 필요
             """)
     @GetMapping("/websocket/info") // 스웨거 웹소켓관련 정보 제공을 위한 api
     public void getWebSocketInfo() {}
