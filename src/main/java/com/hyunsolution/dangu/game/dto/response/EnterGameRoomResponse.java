1e576919f04b16c8b385f3033629d14326e51247
Lee Da-hyeon <dlekgus0205@naver.com>
2025-01-09 03:21:25 +0900

    feat: EnterGameRoomResponse dto생성

---
+++ src/main/java/com/hyunsolution/dangu/game/dto/response/EnterGameRoomResponse.java
@@ -0,0 +1,10 @@
+package com.hyunsolution.dangu.game.dto.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class EnterGameRoomResponse {
+
+        private Boolean isRoomManager;
+
+}
