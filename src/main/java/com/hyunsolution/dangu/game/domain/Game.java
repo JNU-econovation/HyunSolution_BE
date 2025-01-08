1cfe92c8ff839749cf8eb4879e4049a66ff413a6
Lee Da-hyeon <dlekgus0205@naver.com>
2025-01-09 02:46:27 +0900

    feat: Game엔티티 생성

---
+++ src/main/java/com/hyunsolution/dangu/game/domain/Game.java
@@ -0,0 +1,61 @@
+package com.hyunsolution.dangu.game.domain;
+
+import com.hyunsolution.dangu.common.BaseEntity;
+import com.hyunsolution.dangu.user.domain.User;
+import com.hyunsolution.dangu.workspace.domain.Workspace;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Game extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "workspace_id",
+            nullable = false,
+            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Workspace workspace;
+
+    @Column(name ="game_round", nullable = false)
+    @ColumnDefault("1")
+    private Integer gameRound;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private Boolean lose;
+
+    private Integer score;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+
+    @Builder
+    private Game(User user, Workspace workspace, Integer gameRound, LocalDateTime startTime) {
+        this.user = user;
+        this.workspace = workspace;
+        this.gameRound = gameRound;
+        this.startTime = startTime;
+    }
+}
