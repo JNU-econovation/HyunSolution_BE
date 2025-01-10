2ac8be33ac476d97fe3c212729451afad28acf88
Lee Da-hyeon <dlekgus0205@naver.com>
2025-01-10 22:24:37 +0900

    feat: gameRepository생성

--- src/main/java/com/hyunsolution/dangu/game/domain/GameRepository.java
+++ src/main/java/com/hyunsolution/dangu/game/domain/GameRepository.java
@@ -2,4 +2,11 @@ package com.hyunsolution.dangu.game.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Long> {}
+import java.util.List;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+    Game findByWorkspaceIdAndUserId (Long workspaceId, Long userId);
+
+    List<Game> findByWorkspaceId(Long workspaceId);
+}
