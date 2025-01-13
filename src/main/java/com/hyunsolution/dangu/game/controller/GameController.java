ce19ddb42db17cf33b2442aac3187c68d3075668
Lee Da-hyeon <dlekgus0205@naver.com>
2025-01-14 00:31:13 +0900

    feat: api uri 수정 및 대전 목록 조회 api 추가

--- src/main/java/com/hyunsolution/dangu/game/controller/GameController.java
+++ src/main/java/com/hyunsolution/dangu/game/controller/GameController.java
@@ -3,9 +3,11 @@ package com.hyunsolution.dangu.game.controller;
 import com.hyunsolution.dangu.common.apiResponse.ApiResponse;
 import com.hyunsolution.dangu.game.dto.request.GetGameScoreRequest;
 import com.hyunsolution.dangu.game.dto.response.EnterGameRoomResponse;
+import com.hyunsolution.dangu.game.dto.response.GetGameListResponse;
 import com.hyunsolution.dangu.game.service.GameService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +16,18 @@ import org.springframework.web.bind.annotation.*;
 public class GameController {
     private final GameService gameService;
 
-    @GetMapping("/game/{workspaceId}")
+    @GetMapping("/games/{workspaceId}")
     @Operation(
             summary = "게임방에 입장한다.",
             description = "게임방에 입장할 시 방장이라면 당구대 번호 입력창을, 방문자면 대기화면을 띄운다.")
-    public ApiResponse<EnterGameRoomResponse> getGameRoom(
+    public ApiResponse<EnterGameRoomResponse> enterGameRoom(
             @Parameter(hidden = true) @RequestHeader("Authorization") Long userId,
             @PathVariable("workspaceId") Long workspaceId) {
         EnterGameRoomResponse response = gameService.enterGameRoom(workspaceId, userId);
         return ApiResponse.success(response);
     }
 
-    @PostMapping("/game/{workspaceId}") // e.g.  /game/{workspaceId}?tableNumber=5
+    @PostMapping("/games/{workspaceId}") // e.g.  /game/{workspaceId}?tableNumber=5
     @Operation(summary = "당구대 번호를 입력한다.", description = "방장이 입력한 당구대 번호를 저장한다.")
     public ApiResponse<Void> saveTableNumber(
             @Parameter(hidden = true) @RequestHeader("Authorization") Long userId,
@@ -35,7 +37,7 @@ public class GameController {
         return ApiResponse.success(null);
     }
 
-    @PostMapping("/game/{worspaceId}/score")
+    @PostMapping("/games/{worspaceId}/score")
     @Operation(summary = "참여자 별 게임 시작점수와 득점(최종)점수를 입력한다.")
     public ApiResponse<Void> saveScore(
             @Parameter(hidden = true) @RequestHeader("Authorization") Long userId,
@@ -43,5 +45,13 @@ public class GameController {
             @RequestBody GetGameScoreRequest request) {
         gameService.saveGameScore(workspaceId, userId, request);
         return ApiResponse.success(null);
+    }
+
+    @GetMapping("/games")
+    @Operation(summary = "대전 페이지에서 경기목록을 조회한다.")
+    public ApiResponse<List<GetGameListResponse>> getGameList(
+            @Parameter(hidden = true) @RequestHeader("Authorization") Long userId) {
+        List<GetGameListResponse> response = gameService.getGameList(userId);
+        return ApiResponse.success(response);
     }
 }
