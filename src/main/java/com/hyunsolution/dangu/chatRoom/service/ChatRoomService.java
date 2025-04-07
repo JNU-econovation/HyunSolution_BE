fb33d03f3aafa41ce71a9a54003fd41c4fca8bbe
Lee Da-hyeon <dlekgus0205@naver.com>
2025-04-07 15:44:48 +0900

    refactor: 채팅방 존재 여부 검증 및 시간 갱신 파트 코드 분리

---
+++ src/main/java/com/hyunsolution/dangu/chatRoom/service/ChatRoomService.java
@@ -0,0 +1,25 @@
+package com.hyunsolution.dangu.chatRoom.service;
+
+import com.hyunsolution.dangu.chatRoom.domain.ChatRoom;
+import com.hyunsolution.dangu.chatRoom.domain.ChatRoomRepository;
+import com.hyunsolution.dangu.chatRoom.exception.ChatRoomNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    @Transactional
+    public ChatRoom findAndUpdateChatRoom(Long chatRoomId) {
+        ChatRoom chatRoom =
+                chatRoomRepository
+                        .findById(chatRoomId)
+                        .orElseThrow(() -> ChatRoomNotFoundException.EXCEPTION);
+        chatRoom.updateChatTime(); // 채팅 입력 시간에 따른 채팅방 ch_update_at 업데이트
+        chatRoomRepository.save(chatRoom);
+        return chatRoom;
+    }
+}
