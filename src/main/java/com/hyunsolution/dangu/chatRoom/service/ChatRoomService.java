e52b79a2ce5c7c302a3d066bcb455647f2e7891a
Lee Da-hyeon <dlekgus0205@naver.com>
2025-04-09 02:23:30 +0900

    refactor: 메서드 분리 - checkChatRoomMatched 메서드 분리 - validateIsAlreadyMatched 메서드 위치(chatting>chatroom) 이전

--- src/main/java/com/hyunsolution/dangu/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/hyunsolution/dangu/chatRoom/service/ChatRoomService.java
@@ -3,6 +3,7 @@ package com.hyunsolution.dangu.chatRoom.service;
 import com.hyunsolution.dangu.chatRoom.domain.ChatRoom;
 import com.hyunsolution.dangu.chatRoom.domain.ChatRoomRepository;
 import com.hyunsolution.dangu.chatRoom.exception.ChatRoomNotFoundException;
+import com.hyunsolution.dangu.participant.exception.AlreadyMatchedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+
     @Transactional
     public ChatRoom findChatRoom(Long chatRoomId) {
         ChatRoom chatRoom =
@@ -25,6 +27,17 @@ public class ChatRoomService {
     public void updateChatRoom(ChatRoom chatRoom) {
         chatRoom.updateChatTime(); // 채팅 입력 시간에 따른 채팅방 ch_update_at 업데이트
         chatRoomRepository.save(chatRoom);
-        return chatRoom;
+    }
+
+    public void checkChatRoomMatched(ChatRoom chatRoom) {
+        if (chatRoom.getWorkspace().isMatched() && !chatRoom.isMatched()) {
+            throw AlreadyMatchedException.EXCEPTION;
+        }
+    }
+
+    // 해당 메서드의 위치(chatting>chatroom) 및 접근제어자(private>public) 확인
+    public void validateIsAlreadyMatched(Long chatRoomId) {
+        ChatRoom chatRoom = findChatRoom(chatRoomId);
+        checkChatRoomMatched(chatRoom);
     }
 }
