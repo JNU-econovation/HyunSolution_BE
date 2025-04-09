88cd7e906471cbdbb0be06f83607175362bf7e99
Lee Da-hyeon <dlekgus0205@naver.com>
2025-04-09 14:37:09 +0900

    refactor: login 코드 분리 - registerAndLogin과 loginIfMatchPassword 메서드를 따로 분리 - if문 대신 체이닝 방식 활용

--- src/main/java/com/hyunsolution/dangu/user/service/UserService.java
+++ src/main/java/com/hyunsolution/dangu/user/service/UserService.java
@@ -18,17 +18,20 @@ public class UserService {
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Transactional
-    // TODO: 왜 트랜잭션 어노테이션을 붙이는지 공부하기(with flush가 언제 일어나나) - 다현
     public LoginResponse login(String uid, String password) {
-        Optional<User> loginUser = userRepository.findByUid(uid);
-        // 사용자 존재 여부 판단
-        if (loginUser.isEmpty()) {
-            User newUser = registerUser(uid, password);
-            return new LoginResponse(newUser.getId());
-        }
-        // 비밀번호 일치 확인
-        if (isMatchPassword(password, loginUser.get())) {
-            return new LoginResponse(loginUser.get().getId());
+        return userRepository.findByUid(uid)
+                .map(user ->loginIfMatchPassword(user,password) ) //유저 존재시 비번 체크&로그인 처리
+                .orElse(registerAndLogin(uid,password)); //유저가 없으면 회원가입&로그인 처리
+    }
+
+    private LoginResponse registerAndLogin(String uid, String password) {
+        User newUser = registerUser(uid, password);
+        return new LoginResponse(newUser.getId());
+    }
+
+    private LoginResponse loginIfMatchPassword(User user, String password) {
+        if (isMatchPassword(password, user)) {
+            return new LoginResponse(user.getId());
         } else {
             throw UserWrongPasswordException.USER_WRONG_PASSWORD_EXCEPTION;
         }
